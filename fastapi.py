b9b2e494e056c29fe5bda081adac34293c7428c0
JaselinZ <16307100140@fudan.edu.cn>
2023-01-06 16:01:59 -0500

    commit: fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -4,13 +4,33 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+#defign a class to store plan data
+class Plan(BaseModel):
+    plan_id: int
+    plan_name: str
+    plan_detail: List
+    plan_type: str
+    shared: bool
+    status: str
+#define a class to store daily schedule data
+class DailySchedule(BaseModel):
+    date: str
+    day: int
+    company: List
+    schedule: List
 
+#defin a class to store user data
+class User(BaseModel):
+    user_id: int
+    user_name: str
+    user_email: str
+    user_password: str
+    user_vocation: str
 
 app = FastAPI()
 day1 = {'Date': '12-19-2022',
 'Day': 1,
-'Company':[{'name':'Lily', 'id':'001','detail': 'xxx'},
-{'name': 'mike', 'id': '003', 'detail': 'yyy'}],
+'Company':[user1,user2],
 'Schedule':[{'Time': '8-9am', 'event':  'shopping', 'location': '5000 Forbes ave',
 'Detail':'wanna buy a watch'},
 {'Time': '9 - 10am', 'event':  'shopping', 'location': '4342  Murray ave',
@@ -27,10 +47,20 @@ plan1 = {
 'Shared': False,
 'Status': 'active'
 }
-
+plan_list = {plan1,plan2, plan3}
 
 @app.get("/plans/{plan_id}")
 def get_plan_data():
     #get the plan data from database
+    for plan in plan_list:
+            if plan_id == plan['plan_id']:
+                return plan1
+                
+    return 'plan not found'
 
-    return plan1
+
+@app.post("/plans/")
+def create_plan_data():
+    #create a new plan
+    plan3 = {'plan_id': 124,
+'Plan name': 'chicago',}
